aff34b99e81d28bd731f2e9f6185cab2c56ba33d
Burkard Robin <robin.burkard@uzh.ch>
2024-04-16 21:33:05 +0200

    #[no specific issue] GameControllerTest not working

--- src/main/java/ch/uzh/ifi/hase/soprafs24/Application.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/Application.java
@@ -2,17 +2,21 @@ package ch.uzh.ifi.hase.soprafs24;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+
+@EnableJpaRepositories
 @RestController
 @SpringBootApplication
 public class Application {
-
+  
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
   }
