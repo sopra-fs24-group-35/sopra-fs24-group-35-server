0ed4f27f73df47eb6ffe202eb306ace58886f767
Burkard Robin <robin.burkard@uzh.ch>
2024-04-22 15:04:18 +0200

    #69: Added more GameService tests

--- src/test/java/ch/uzh/ifi/hase/soprafs24/service/GameServiceTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs24/service/GameServiceTest.java
@@ -16,11 +16,13 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.server.ResponseStatusException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class GameServiceTest {
 
@@ -162,6 +164,109 @@ public class GameServiceTest {
     }
 
 
+    @Test
+    public void executeAttack_validInputTwoAttacks_updatedGame() {
 
+        // given territories
+        Territory attacking = testGame.getBoard().getTerritories().get(0); //Territory called 'Paradeplatz'
+        Territory defending = testGame.getBoard().getTerritories().get(1); //Territory called 'Central'
+
+        // given attack entity
+        Attack attack = new Attack();
+        attack.setTroopsAmount(3);
+
+        
+        Random random = new Random();
+
+        // perform the attack method
+        Game afterAttack = gameService.executeAttack(testGame, attack, attacking, defending, random);
+
+        // Check if defender (Central) and attacker (Central) have lost 2 troops in total
+        // Since both had 7 troops initially, they should now have 12 troops when added together
+        assertEquals(12, afterAttack.getBoard().getTerritories().get(0).getTroops()
+        + afterAttack.getBoard().getTerritories().get(1).getTroops());
+
+    }
+
+    @Test
+    public void executeAttack_validInputOneAttack_updatedGame() {
+
+        // given territories
+        Territory attacking = testGame.getBoard().getTerritories().get(0); //Territory called 'Paradeplatz'
+        Territory defending = testGame.getBoard().getTerritories().get(1); //Territory called 'Central'
+
+        // given attack entity whist states to execute only one attack
+        Attack attack = new Attack();
+        attack.setTroopsAmount(1);
+
+        
+        Random random = new Random();
+
+        // perform the attack method
+        Game afterAttack = gameService.executeAttack(testGame, attack, attacking, defending, random);
+
+        // Check if defender (Central) or attacker (Central) has lost 1 troop
+        // Since both had 7 troops initially, they should now have 13 troops when added together
+        assertEquals(13, afterAttack.getBoard().getTerritories().get(0).getTroops()
+        + afterAttack.getBoard().getTerritories().get(1).getTroops());
+
+
+        
+    }
+
+    @Test
+    public void executeRepeatedAttacks_validInput_updatedGame() {
+
+        // given territories
+        Territory attacking = testGame.getBoard().getTerritories().get(0); //Territory called 'Paradeplatz'
+        Territory defending = testGame.getBoard().getTerritories().get(1); //Territory called 'Central'
+
+        // given attack entity whist states to execute only one attack
+        Attack attack = new Attack();
+        attack.setAttackingTerritory("Paradeplatz");
+        attack.setDefendingTerritory("Central");
+        attack.setRepeats(3);
+        attack.setTroopsAmount(2);
+
+        // mock repository
+        Mockito.when(gameRepository.getByGameId(Mockito.any())).thenReturn(testGame);
+
+        // perform the attack method
+        Game afterAttack = gameService.executeRepeatedAttacks(attack, 1L);
+
+        // Check if defender (Central) and attacker (Central) have lost 6 troops in total (3 attacks with 2 troops each)
+        // Since both had 7 troops initially, they should now have 8 troops when added together
+        assertEquals(8, afterAttack.getBoard().getTerritories().get(0).getTroops()
+        + afterAttack.getBoard().getTerritories().get(1).getTroops());
+
+    }
+
+    @Test
+    public void getTerritory_validInput_success() {
+
+        //when
+        Mockito.when(gameRepository.getByGameId(Mockito.any())).thenReturn(testGame);
+
+        Territory territory = gameService.getTerritory(1L,"Paradeplatz");
+
+        //then
+        assertEquals(territory.getName(), "Paradeplatz");
+
+    }
+
+    @Test
+    public void getTerritory_invalidInput_error() {
+
+        //when
+        Mockito.when(gameRepository.getByGameId(Mockito.any())).thenReturn(testGame);
+
+        //then
+        assertThrows(ResponseStatusException.class, () -> {       
+            gameService.getTerritory(1L, "Bellvue");     
+        } );
+
+    }
 
 }
+
+
